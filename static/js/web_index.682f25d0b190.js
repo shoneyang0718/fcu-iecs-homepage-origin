ca64731b9f6644d25bf4d7575622a2998c1029d7
shone0718 <d1349803@o365.fcu.edu.tw>
2026-03-24 21:31:45 +0800

    Improve Lighthouse red issues: a11y, SEO, JS stability, and image loading

--- static/js/web_index.682f25d0b190.js
+++ static/js/web_index.682f25d0b190.js
@@ -17,9 +17,23 @@ var swiper = new Swiper(".mySwiper", {
 document.querySelectorAll('.index-video').forEach(element => {
     element.addEventListener('click', () => {
         swiper.autoplay.stop();
-        let url = element.dataset.url;
-        url = url.split('v=')[1];
-        document.querySelector('#modalForVideo .modal-content').innerHTML = `<iframe src="https://www.youtube.com/embed/${url}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        const rawUrl = element.dataset.url || '';
+        let videoId = '';
+        try {
+            const parsed = new URL(rawUrl);
+            videoId = parsed.searchParams.get('v') || '';
+            if (!videoId && parsed.pathname) {
+                videoId = parsed.pathname.split('/').filter(Boolean).pop() || '';
+            }
+        } catch (error) {
+            videoId = '';
+        }
+
+        if (!videoId) {
+            return;
+        }
+
+        document.querySelector('#modalForVideo .modal-content').innerHTML = `<iframe src="https://www.youtube.com/embed/${videoId}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     });
 });
 
@@ -30,6 +44,14 @@ document.querySelector('#modalForVideo').addEventListener('hide.bs.modal', () =>
 
 window.addEventListener('load', () => {
     let index_about = document.querySelector('.index-about-background');
-    let bgimg = index_about.dataset.bgimg;
-    document.getElementsByClassName('index-about-background')[0].style.backgroundImage = `url(${location.origin + bgimg})`;
+    if (index_about && index_about.dataset && index_about.dataset.bgimg) {
+        let bgimg = index_about.dataset.bgimg;
+        document.getElementsByClassName('index-about-background')[0].style.backgroundImage = `url(${location.origin + bgimg})`;
+    }
+
+    document.querySelectorAll('#carousel .carousel-inner > a').forEach((anchor, index) => {
+        if (!anchor.getAttribute('aria-label')) {
+            anchor.setAttribute('aria-label', `查看第${index + 1}則公告`);
+        }
+    });
 });
